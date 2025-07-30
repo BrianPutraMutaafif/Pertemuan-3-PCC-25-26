72535511f3a06a086e0b593a29e1c3fe230edb0e
zore836 <shwewmaddes@gmail.com>
2025-07-30 12:42:38 +0700

    fitur splash commit

--- app/src/main/java/com/smktunas/app2_colab/SplashActivity.kt
+++ app/src/main/java/com/smktunas/app2_colab/SplashActivity.kt
@@ -1,4 +1,20 @@
 package com.smktunas.app2_colab
 
-class SplashActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Delay 3 detik lalu pindah ke MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
 }
